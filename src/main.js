343582140054b40ff0d513b4f8e73d87bb6fcc23
Antoine B <aberthillot@myges.fr>
2025-04-02 12:16:28 +0200

    Main page done

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
+let cart = {};
+let product_list = []
+let displayed_product_list = []
 
 
 /**
+ * @typedef Product
+ * @property {String} nom
+ * @property {Number} quantite_stock
+ * @property {Number} prix_unitaire
+ */
+
+/**
+ * Load cart from localStorage, or creates new cart
+ */
+function loadCart() {
+    let existing_cart = JSON.parse(localStorage.getItem('cart'));
+    if (existing_cart) {
+        cart = existing_cart;
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+/**
  * Load a list of product from the path given. If no file is found, return an empty list.
- * @param path The path of the file to load.
+ * @param path {String} The path of the file to load.
  * @returns {Promise<Array>} Promise of the file loading
  */
 function loadProductsFile(path) {
@@ -19,8 +40,26 @@ function loadProductsFile(path) {
 }
 
 /**
+ * Adds an entry in the cart for the product, then add it to the cart in localStorage
+ * @param productName {String}
+ */
+function addToCart(productName) {
+    if (!productName || !productName in product_list) {
+        return;
+    }
+
+    if (!cart[productName]) {
+        cart[productName] = 0;
+    }
+
+    cart[productName]++;
+    localStorage.setItem("cart", JSON.stringify(cart));
+    console.log(JSON.parse(localStorage.getItem("cart")));
+}
+
+/**
  * Create a product card HTML element for the product in parameter
- * @param product A product
+ * @param product {Product} A product
  * @returns {HTMLLIElement|null}
  */
 function createProductCard(product) {
@@ -37,22 +76,96 @@ function createProductCard(product) {
     <button class="card-action">Ajouter à la liste</button>
     `
 
+    const button = card.querySelector('.card-action');
+    button.addEventListener('click', function() {
+        addToCart(product.nom);
+    });
+
     return card;
 }
 
 /**
- * Display a list of product into a HTML element with id "product-list"
- * @param products An array of product
+ * Display a list of product into an HTML element with id "liste-produits"
+ * @param products {Array<Product>}An array of product
  */
 function displayProducts(products) {
-    let list = document.getElementById("product-list");
+    let list = document.getElementById("liste-produits");
+    let count = document.getElementById("compteur-produits");
 
     if (!list) return;
+
+    while (list.lastElementChild) {
+        list.removeChild(list.firstChild);
+    }
 
     for (const product of products) {
         list.append(createProductCard(product));
     }
+
+    count.textContent = `${products.length} produits`;
 }
 
-loadProductsFile("public/liste_produits_quotidien.json").then((res) => displayProducts(res));
+/**
+ * Sort product list and re-display them by select with id "tri"
+ */
+function sortProducts() {
+    let sortValue = document.getElementById("tri").value;
+
+    if (!sortValue in ["nom", "prix"]) {
+        return;
+    }
+    if (sortValue === "prix") {
+        displayed_product_list.sort((p1, p2) => p1.prix_unitaire - p2.prix_unitaire);
+    }
+    if (sortValue === "nom") {
+        displayed_product_list.sort((p1, p2) => p1.nom.localeCompare(p2.nom));
+    }
+
+    displayProducts(displayed_product_list);
+}
+
+/**
+ * Load products in HTML
+ */
+function loadProducts() {
+    loadProductsFile("public/liste_produits_quotidien.json").then((res) => {
+        product_list = res;
+        displayed_product_list = res;
+
+        displayProducts(displayed_product_list);
+    });
+}
+
+/**
+ * Reset filters (and reload product list)
+ */
+function resetFilter() {
+    document.getElementById("tri").value = "";
+    document.getElementById("recherche").value = "";
+
+    displayed_product_list = product_list;
+
+    displayProducts(displayed_product_list);
+}
+
+/**
+ * Apply search parameter on input with id "recherche" to product list
+ */
+function searchProducts() {
+    let searchValue = document.getElementById("recherche").value;
+
+    displayed_product_list = displayed_product_list.filter((p) => p.nom.includes(searchValue))
+
+    displayProducts(displayed_product_list);
+}
+
+loadCart();
+
+document.getElementById("recherche").addEventListener("input", searchProducts);
+document.getElementById("tri").addEventListener("change", sortProducts);
+document.getElementById("#reset-filtres").addEventListener("click", resetFilter);
+
+resetFilter();
+loadProducts();
+
 
